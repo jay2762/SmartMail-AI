68b050f433568eaef291211bbefa1f85afe17582
Jay Shah <121171971+jay2762@users.noreply.github.com>
2025-07-12 19:21:39 -0500

    Add files via upload

---
+++ Email Assistant Backend - SB/src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,27 @@
+package com.email.writer.app;
+
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+    public String getTone() {
+        return tone;
+    }
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    // Optionally: add setters if needed
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+}
